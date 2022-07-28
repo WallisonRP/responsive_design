6b8449470e81b91451c18327e82acaed3e0569aa
Wallyz <80826888+WallisonRP@users.noreply.github.com>
2022-07-27 21:41:14 -0300

    animate mobile menu

--- Flex/menu-flex/navbar.js
+++ Flex/menu-flex/navbar.js
@@ -8,8 +8,19 @@ class MobileNavBar {
     this.handleClick = this.handleClick.bind(this);
   }
 
+  animateLinks() {
+    this.navLinks.forEach(
+      (link, index) => {
+        link.style.animation 
+        ? (link.style.animation = "") 
+        : (link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 +0.3}s`)
+      }
+    );
+  }
+
   handleClick() {
     this.navList.classList.toggle(this.activeClass);
+    this.animateLinks();
   }
 
   addClickEvent() {
